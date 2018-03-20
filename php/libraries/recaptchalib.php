749d08fff6086ff46dfa77e60d6467057fa88cad
Tim Traver <timtraver@gmail.com>
2018-03-19 23:11:51 -0700

    debugging captcha

--- php/libraries/recaptchalib.php
+++ php/libraries/recaptchalib.php
@@ -40,8 +40,7 @@ class ReCaptchaResponse
 class ReCaptcha
 {
     private static $_signupUrl = "https://www.google.com/recaptcha/admin";
-    private static $_siteVerifyUrl =
-        "https://www.google.com/recaptcha/api/siteverify?";
+    private static $_siteVerifyUrl = "https://www.google.com/recaptcha/api/siteverify?";
     private $_secret;
     private static $_version = "php_1.0";
     /**
@@ -123,6 +122,10 @@ class ReCaptcha
             $recaptchaResponse->success = false;
             $recaptchaResponse->errorCodes = $answers[error-codes];
         }
+        print "<!--".$this->_secret."-->\n";
+        print "<!--".$response."-->\n";
+        print "<!--".$recaptchaResponse->success."-->\n";
+        
         return $recaptchaResponse;
     }
 }
