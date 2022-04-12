274e309819533a63b7afd1e4965d98ec9b5f4f76
Tim Traver <timtraver@gmail.com>
2022-04-12 22:13:01 +0000

    Update the self scoring to auto login to the user

--- htdocs/se.php
+++ htdocs/se.php
@@ -12,6 +12,30 @@ require_once("../php/conf.php");
 
 include_library('functions.inc');
 
+# Let us auto log in the pilot before we send them to the self score. That way they don't need to log into the vault
+# let's get the user info
+$stmt = db_prep( "
+	SELECT *
+	FROM event_pilot ep
+	LEFT JOIN pilot p ON ep.pilot_id
+	LEFT JOIN user u ON p.user_id = u.user_id
+	WHERE ep.event_pilot_id = :event_pilot_id
+		AND u.user_status = 1
+" );
+$result = db_exec( $stmt, array(
+	"event_pilot_id" => $_REQUEST['p']
+) );
+$user = $result[0];
+
+$path = "/";
+$host = $_SERVER['HTTP_HOST'];
+# New session stuff
+create_fsession($path,$host);
+$fsession['auth'] = TRUE;
+$fsession['user_id'] = $user['user_id'];
+$fsession['user_name'] = $user['user_name'];
+save_fsession();
+
 $location = "http://www.f3xvault.com/?action=event&function=event_self_entry&event_id=" . $_REQUEST['e'] . "&event_pilot_id=" . $_REQUEST['p'];
 header( "Location: $location");
 exit;
